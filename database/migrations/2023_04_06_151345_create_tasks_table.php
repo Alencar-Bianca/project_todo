b8cee00707fe992d6464f1dc7d451bafeb3b3886
Alencar-Bianca <alencar-bianca@hotmail.com>
2023-04-06 22:26:42 -0300

    create migrates

---
+++ database/migrations/2023_04_06_151345_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\{User, Category};
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable(false);
+            $table->string('description');
+            $table->dateTime('due_date');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onDelete('CASCADE');
+            $table->foreignIdFor(Category::class)->references('id')->on('categories')->on('users')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeignIdFor(User::class);
+            $table->dropForeignIdFor(Category::class);
+        });
+        Schema::dropIfExists('tasks');
+    }
+};
